6ddc68ca426a2df61517a3b81fa8c7d345ee7cba
YusufAbdrabbo <47288146+YusufAbdrabbo@users.noreply.github.com>
2021-01-31 10:19:02 -0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,41 @@
+# BabySearchSuggestionSystem
+
+An exploratory-purpose CLI program that recreates the following idea:
+
+A web browser has a search field where the user can enter a keyword, and clicking a confirmation button so it will redirect to another page with the results of search. When some string is entered in the search field, the program displays, below, some options to auto complete this string according to the searches already performed by the user.
+
+For instance, if the words "algoritmos" and "algas" have just been looked, when typing the string "alg", the program ought to propose the words "algorithms" and "algas". Subsequently, for each string entered, the program ought to suggest recently looked words prefixed with this string. In the event that any word is exactly equivalent to the typed string, it ought to likewise be suggested.
+
+Our BabySearchSuggestionSystem is inspired by the portrayed idea above, thus, It is concerned about the amount of words that will be suggested, and the maximum size (length) they can reach. So the program is proposed to be given a couple of words previously searched, and a series of inquiries (are being searched) made out of strings, at that point utilizing these, it shows the number of words the program ought to suggest, and the length of the biggest of these words.
+
+# Sample Case:
+
+**Exact form of how inputs are entered in the program:**
+
+5
+
+maratonaicpc
+
+marathon
+
+programacao
+
+progress inputs
+
+3
+
+marat
+
+programacao
+
+outputs
+
+
+
+**Exact form of its output:**
+
+2 12
+
+1 11
+
+-1 
---
+++ src/babysearchsuggestionsystem/BabySearchSuggestionSystem.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package babysearchsuggestionsystem;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Yusuf
+ */
+public class BabySearchSuggestionSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        // TODO code application logic here
+        Scanner scanner1 = new Scanner(System.in); //Declaring object scanner1 to help later getting inputs.
+     String[] aSW = new String [N+1]; //Declaring array aSW to store Already Searched Words 'Making sure the number of elements in the array (its lenth) is the same number of Already Searched Words.
+     short N = scanner1.nextShort(); //Initializing N for the number of Already Searched Words 'value to be assigned to N is taken from user'.
+     for(short i=0;i<=N;i++) //An iteration statement to get and assign a string value from the user to each element of aSW array."
+     {
+         aSW[i]=scanner1.nextLine().toLowerCase();
+     }
+     // END OF WORK REGARDING getting Already Searched Words.
+     
+     byte Q = scanner1.nextByte(); //Initializing Q for the number of search queries (terms) 'value to be assigned to Q is taken from user'.
+     String[] sQ = new String[Q+1]; //Declaring array sQ to store Search Queries 'Making sure the number of elements in the array (its lenth) is the same number of search queries.
+     for(byte i2=0;i2<=Q;i2++) //An iteration statement to get and assign a string value from the user to each element of sQ array."
+     {
+         sQ[i2]=scanner1.nextLine().toLowerCase();
+     }
+     // END OF WORK REGARDING getting current Search Queries.
+     
+     
+         
+         
+    }
+    
+}
