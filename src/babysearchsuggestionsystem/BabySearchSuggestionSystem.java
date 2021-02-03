4c2ceb7f1a2e9e28eb79447298a553bab4ca2855
Mohamed Kamal Abdelaziz Ahmed <77943267+algndy@users.noreply.github.com>
2021-02-03 20:04:55 +0200

    Update BabySearchSuggestionSystem.java
    1) Add a function to fill in the array
    2) Format some parts of the code, such as variables
    3) Completion of the minus of the code

--- src/babysearchsuggestionsystem/BabySearchSuggestionSystem.java
+++ src/babysearchsuggestionsystem/BabySearchSuggestionSystem.java
@@ -9,37 +9,71 @@ import java.util.Scanner;
 
 /**
  *
- * @author Yusuf
+ * @author Yusuf Ibrahim Abdrabbo - linkedin.com/in/yusufabdrabbo
+ * @collaborator Mohamed Kamal Abdelaziz - linkedin.com/in/mohamedkamal-in
  */
 public class BabySearchSuggestionSystem {
 
     /**
      * @param args the command line arguments
      */
+    static Scanner scanner1 = new Scanner(System.in); //Declaring object scanner1 to help later getting inputs.
     public static void main(String[] args) 
     {
-        // TODO code application logic here
-        Scanner scanner1 = new Scanner(System.in); //Declaring object scanner1 to help later getting inputs.
-     short N = scanner1.nextShort(); //Initializing N for the number of Already Searched Words 'value to be assigned to N is taken from user'.   
-     String[] aSW = new String [N+1]; //Declaring array aSW to store Already Searched Words 'Making sure the number of elements in the array (its lenth) is the same number of Already Searched Words.
-     
-     for(short i=0;i<=N;i++) //An iteration statement to get and assign a string value from the user to each element of aSW array."
-     {
-         aSW[i]=scanner1.nextLine().toLowerCase();
-     }
-     // END OF WORK REGARDING getting Already Searched Words.
-     
-     byte Q = scanner1.nextByte(); //Initializing Q for the number of search queries (terms) 'value to be assigned to Q is taken from user'.
-     String[] sQ = new String[Q+1]; //Declaring array sQ to store Search Queries 'Making sure the number of elements in the array (its lenth) is the same number of search queries.
-     for(byte i2=0;i2<=Q;i2++) //An iteration statement to get and assign a string value from the user to each element of sQ array."
-     {
-         sQ[i2]=scanner1.nextLine().toLowerCase();
-     }
-     // END OF WORK REGARDING getting current Search Queries.
-     
-     
-         
-         
+       //VARIABLES
+        
+        short N; //N for the number of Already Searched Words
+        byte Q; //Q for the number of search queries (terms)
+        
+        //PROCESSING
+        
+        // START OF WORK REGARDING getting Already Searched Words.
+        N = scanner1.nextShort(); //Initializing N for the number of Already Searched Words 'value to be assigned to N is taken from user'.   
+        String[] aSW = new String [N]; //Declaring array aSW to store Already Searched Words 'Making sure the number of elements in the array (its lenth) is the same number of Already Searched Words.
+        fillarray(aSW);//Method to get and assign a string value from the user to each element of aSW array."
+        // END OF WORK REGARDING getting Already Searched Words.
+
+        // START OF WORK REGARDING getting current Search Queries.
+         Q = scanner1.nextByte(); //Initializing Q for the number of search queries (terms) 'value to be assigned to Q is taken from user'.
+         String[] sQ = new String[Q]; //Declaring array sQ to store Search Queries 'Making sure the number of elements in the array (its lenth) is the same number of search queries.
+          fillarray(sQ);//Method to get and assign a string value from the user to each element of sQ array."
+        // END OF WORK REGARDING getting current Search Queries.
+
+         for(int i=0;i<sQ.length;i++)//OUTER (FOR LOOP)
+            {
+                    int ans=0,len=0; //ans is he number of repetition of the word & len is the number of letters is the largest word among the repeated words
+
+                    for(int j=0;j<aSW.length;j++)//INER (FOR LOOP)
+                    {      
+
+                            if(aSW[j].startsWith(sQ[i]))
+                            {
+                                ans++;
+                                len=Math.max(len, aSW[j].length());
+                            }
+
+                    }//END OF INER (FOR LOOP)
+                   if(ans==0)  
+                   {
+                       System.out.println("-1");
+                   }
+                   else
+                   {
+                       System.out.println(ans+" "+len);  
+                   }
+            }//END OF OUTER (FOR LOOP)
+
+
     }
+    //////////////////////////FUNCTIONS//////////////////////////////////////////
+    //////////////////////////FUNCTIONS//////////////////////////////////////////
     
+    //START fillarray method
+    public static void fillarray(String[]namearray)//this method to fill the array
+    {
+        for(short i=0;i<namearray.length;i++)//An iteration statement to get and assign a string value from the user to each element of aSW array and sQ array.
+        {
+            namearray[i]=scanner1.next().toLowerCase();
+        }
+    }//END fillarray method  
 }
